6e2d147891322466d770061088a35b7421edfac0
mikekwizera <mikekwizera4@gmail.com>
2025-01-03 05:21:18 +0200

    Define website home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,20 +1,34 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
+import {Head, Link} from '@inertiajs/vue3';
+import GroupList from "@/Components/app/GroupList.vue";
+import FollowingList from "@/Components/app/FollowingList.vue";
+import CreatePost from "@/Components/app/CreatePost.vue";
+import PostList from "@/Components/app/PostList.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
-defineProps({
-
-});
-
-function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
-}
+defineProps({});
 </script>
 
 <template>
-    <Head title="MkShopify" />
 
-    Test
+    <Head title="MkShopify Social Media Website"/>
+
+            <AuthenticatedLayout>
+                <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+                    <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                        <GroupList/>
+                    </div>
+                    <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                        <FollowingList/>
+                    </div>
+                    <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
+                        <CreatePost/>
+                        <PostList class="flex-1"/>
+                    </div>
+                </div>
+
+        </AuthenticatedLayout>
 </template>
+
+<style>
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Home', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->middleware(['auth', 'verified']);
+//Route::get('/', function () {
+//    return Inertia::render('Home', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+//})->middleware(['auth', 'verified']);
+
+Route::get('/', [HomeController::class, 'index'])
+    ->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
