8e2c9443c3552004d5921d065797328ecff39110
mikekwizera <mikekwizera4@gmail.com>
2025-01-08 04:57:54 +0200

    implements updating profile details

--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -72,23 +72,14 @@
                     </div>
                     <div class="flex justify-between items-center flex-1 p-4">
                         <h2 class="font-bold text-lg">{{ user.name }}</h2>
-                        <PrimaryButton v-if="isMyProfile">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" class="w-4 h-4 mr-2">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"/>
-                            </svg>
-                            Edit Profile
-                        </PrimaryButton>
+
                     </div>
                 </div>
             </div>
             <div class="border-t">
                 <TabGroup>
                     <TabList class="flex bg-white">
-                        <Tab v-if="isMyProfile" v-slot="{ selected }" as="template">
-                            <TabItem text="About" :selected="selected"/>
-                        </Tab>
+
                         <Tab v-slot="{ selected }" as="template">
                             <TabItem text="Posts" :selected="selected"/>
                         </Tab>
@@ -101,11 +92,12 @@
                         <Tab v-slot="{ selected }" as="template">
                             <TabItem text="Followings" :selected="selected"/>
                         </Tab>
+                        <Tab v-if="setting" v-slot="{ selected }" as="template">
+                            <TabItem text="Setting" :selected="selected"/>
+                        </Tab>
                     </TabList>
                     <TabPanels class="mt-2">
-                        <TabPanel v-if="isMyProfile">
-                            <Edit :must-verify-email="mustVerifyEmail" :status="status"/>
-                        </TabPanel>
+
                         <TabPanel class="bg-white p-3 shadow">
                             Posts
                         </TabPanel>
@@ -117,6 +109,9 @@
                         </TabPanel>
                         <TabPanel class="bg-white p-3 shadow">
                             Followings
+                        </TabPanel>
+                        <TabPanel v-if="setting">
+                            <Edit :must-verify-email="mustVerifyEmail" :status="status"/>
                         </TabPanel>
                     </TabPanels>
                 </TabGroup>
@@ -143,7 +138,7 @@ const showNotification = ref(true)
 const coverImageSrc = ref('')
 const avatarImageSrc = ref('')
 const authUser = usePage().props.auth.user;
-const isMyProfile = computed(() => authUser && authUser.id === props.user.id)
+const setting = computed(() => authUser && authUser.id === props.user.id)
 const props = defineProps({
     errors: Object,
     mustVerifyEmail: {
